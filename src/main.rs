ae427229434c3b0663973cac55daf97b0856e457
Joseph LeBlanc <joseph.t.leblanc@protonmail.com>
2025-03-07 23:42:01 +0000

    fix: update RON_SAVE_FILE constant path

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,12 @@ use crate::parser::save_graph;
 use std::path::Path;
 
 const TARGET_FILE: &str = "/home/brasides/code/rag_workspace/example_traverse_target/src/main.rs";
+const RON_SAVE_FILE: &str =
+    "/home/brasides/code/rag_workspace/example_traverse_target/data/graph.ron";
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let target_path = Path::new(TARGET_FILE);
-    let save_path = Path::new("./data/graph.ron");
+    let save_path = Path::new(RON_SAVE_FILE);
 
     let code_graph = analyze_code(target_path)?;
     Ok(save_graph(&code_graph, save_path)?)
